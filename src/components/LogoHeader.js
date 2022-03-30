55c6df7ba31ff1557e71d1f4ecbc1c0771c563ee
Jay <jbarbanel531@gmail.com>
2022-03-30 13:51:52 -0700

    updayed logo, placed it center, added background color

--- src/components/LogoHeader.js
+++ src/components/LogoHeader.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import logo from '../images/logo.webp'
+import logo from '../images/logo.webp';
+import '../App.css';
 
 function LogoHeader() {
   return (
-    <Card>
-      <Card.Img variant="top" src={logo} />
+    <Card className="cardOne">
+      <Card.Img className="logo" variant="top" src={logo} />
       <Card.Body>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+     
       </Card.Body>
     </Card>
   );
