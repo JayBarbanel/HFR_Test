2059480a505c8858dfb2a7d3cccccd00aaa42c35
Jay <jbarbanel531@gmail.com>
2022-03-31 14:25:38 -0700

    added image and copy to attention component.  Need to add font and position it better

--- src/components/Attention.js
+++ src/components/Attention.js
@@ -4,26 +4,26 @@ import {
     CardGroup,
     Card,
 }from 'react-bootstrap';
-import jacket from '../images/jckt.jpg';
+import larow from '../images/larow.jpg';
 
 
 
 function Attention (){
     return (
-<CardGroup className="cardTwo" style={{display: 'flex', flexDirection: 'row'}}>
+<CardGroup className="cardThree" style={{display: 'flex', flexDirection: 'row'}}>
   <Card style={{flex: 1}}>
-    <Card.Img variant="top" src={jacket} />
-  </Card>
-  <Card style={{flex: 1}}>
-    <Card.Body>
-      <Card.Title className="title">Honesty</Card.Title>
+  <Card.Body>
+      <Card.Title className="title">Personalized Attention</Card.Title>
       <Card.Text className="bodytxt">
-      Guided by the objective of "preservation of life and property," we encompass the same values of safeguarding your future and protecting you and your families' income. <br/>It is the trust and honesty that runs deep in the first responders' culture that is also embedded in our values. 
+      At our company, we are fully committed to providing our Heroes and First Responders with the highest quality insurance plans available combined with some of the lowest possible rates.  We value the long-term relationship with our clients, and are proud to provide individuals, families, and businesses the personal attention when it matters the most.  We pride ourselves in processing claims ensuring our client's needs are fully met reducing the stress level.   
       </Card.Text>
     </Card.Body>
     <Card.Footer>
       <small className="text-muted">Last updated 3 mins ago</small>
     </Card.Footer>
+  </Card>
+  <Card style={{flex: 1}}>
+  <Card.Img className="larow" variant="top" src={larow} />
   </Card>
  
 </CardGroup>
