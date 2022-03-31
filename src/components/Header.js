968e11b6fdb8e6d97d0d82ccd1e9058f7cd935f7
Jay <jbarbanel531@gmail.com>
2022-03-31 11:09:12 -0700

    added header copy, need to stylize font, make images smaller & change our LAFD city to county

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,42 +14,16 @@ function Header(){
 <CardGroup style={{display: 'flex', flexDirection: 'row'}}>
   <Card style={{flex: 1}}>
     <Card.Img variant="top" src={LAFD} />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
   </Card>
   <Card style={{flex: 1}}>
-    <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        You take care of emergencies, we take care of yours{' '}
+      <Card.Text className="weTake">
+        You take care of emergencies...<br/> We take care of yours.{' '}
       </Card.Text>
     </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
   </Card>
   <Card style={{flex: 1}}>
     <Card.Img variant="top" className="lapd" src={LAPD} />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
   </Card>
 </CardGroup>
     )
