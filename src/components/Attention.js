0e6fb1b4e85ffd75c35e6fbdba4a5481062810b6
Jay <jbarbanel531@gmail.com>
2022-04-01 10:42:31 -0700

    removed all footer text from x3 cards

--- src/components/Attention.js
+++ src/components/Attention.js
@@ -18,9 +18,6 @@ function Attention (){
       At our company, we are fully committed to providing our Heroes and First Responders with the highest quality insurance plans available combined with some of the lowest possible rates.  We value the long-term relationship with our clients, and are proud to provide individuals, families, and businesses the personal attention when it matters the most.  We pride ourselves in processing claims ensuring our client's needs are fully met reducing the stress level.   
       </Card.Text>
     </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
   </Card>
   <Card style={{flex: 1}}>
   <Card.Img className="larow" variant="top" src={larow} />
